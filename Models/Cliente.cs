8a4cff3b8535ba73489524bc79e1e1fe8ceca665
JaraClaudioEz <claudiojara848@gmail.com>
2022-03-17 20:01:08 -0300

    Primer setup del proyecto

---
+++ Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PruebaTecnicaBitsion.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Genero { get; set; }
+        public bool Estado { get; set; }
+        public bool Maneja { get; set; }
+        public bool Lentes { get; set; }
+        public bool Diabetico { get; set; }
+        public string Enfermedades { get; set; }
+
+
+
+    }
+}
